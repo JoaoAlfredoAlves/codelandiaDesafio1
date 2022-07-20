1d5d3fa08c11867f2cab33888a0c6081572a3d5d
João Alfredo Alves <joaoadsousa777@gmail.com>
2022-07-20 14:22:31 -0300

    Adicionando animações ao scrollar a página

---
+++ assets/script/scripts.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "25px",
+  duration: 500,
+  reset: true,
+});
+
+ScrollReveal().reveal(".article", { delay: 75 });
+ScrollReveal().reveal(".div-data-heart", { delay: 125 });
+ScrollReveal().reveal(".article-title", { delay: 125 });
+ScrollReveal().reveal(".article-content", { delay: 125 });
